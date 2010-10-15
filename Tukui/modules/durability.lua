f6a04febb7015bd801882ca8bc3f6071d36ae4bf
Eclipse- <sheppard.sam@gmail.com>
2010-10-15 01:21:03 +0100

    Sigh.

--- Tukui/modules/durability.lua
+++
@@ -1,8 +0,0 @@
--- move durability frame.
-
-hooksecurefunc(DurabilityFrame,"SetPoint",function(self,_,parent)
-    if (parent == "MinimapCluster") or (parent == _G["MinimapCluster"]) then
-        self:ClearAllPoints()
-		self:SetPoint("TOP", TukuiMinimap, "BOTTOM", 0, -20);
-    end
-end)
